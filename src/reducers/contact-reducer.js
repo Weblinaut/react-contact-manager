703ec554f10a51bff106362b1b6dd36b1ac0178e
Weblinaut <skydiver76@gmail.com>
2018-06-19 16:45:07 -0400

    working form

--- src/reducers/contact-reducer.js
+++ src/reducers/contact-reducer.js
@@ -3,12 +3,51 @@ const defaultState = {
 }
 export default (state=defaultState, action={}) => {
     switch (action.type) {
-        case 'FETCH_CONTACTS': {
+        /*const defaultState = {
+            contacts: [],
+            contact: {name:{}},
+            loading: false,
+            errors: {} }*/
+        case 'FETCH_CONTACTS_FULFILLED': {
             return {
                 ...state,
-                contacts: action.payload
+                contacts: action.payload.data.data || action.payload.data
             }
-        } default:
+        }
+        case 'NEW_CONTACT': {
+            return {
+                ...state,
+                contact: {name:{}}
+            }
+        }
+        case 'SAVE_CONTACT_PENDING': {
+            return {
+                ...state,
+                loading: true
+            }
+        }
+        case 'SAVE_CONTACT_FULFILLED': {
+            return {
+                ...state,
+                contacts: [...state.contacts, action.payload.data],
+                errors: {},
+                loading: false
+            }
+        }
+        case 'SAVE_CONTACT_REJECTED': {
+            const data = action.payload.response.data;
+            const { "name.first":first, "name.last":last, phone, email
+                } = data.errors;
+            const errors = { global: data.message, name: { first,last },  phone, email };
+            return {
+                ...state,
+                errors: errors,
+                loading: false
+            }
+        }
+            "name.first"
+
+        default:
         return state;
     }
 }
