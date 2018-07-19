c332876727c8dda8dc08a784fb0b4d1137e75dd2
Weblinaut <skydiver76@gmail.com>
2018-07-19 11:03:02 -0400

    connecting the dots

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const client = axios.create({
-    baseURL: "http://localhost:3030",
+    baseURL: process.env.BACKEND_URI || "http://localhost:3030",
     headers: {
         "Content-Type": "application/json"
     }
