ec849ed90d04cad8a6b85ce22d6215b9fb3283ff
Weblinaut <skydiver76@gmail.com>
2018-07-19 10:56:19 -0400

    added production mongodb

--- backend/src/mongoose.js
+++ backend/src/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 module.exports = function () {
   const app = this;
 
-  mongoose.connect(app.get('mongodb'), {
+  mongoose.connect(process.env.MONGODB_URI || app.get('mongodb'), {
     useMongoClient: true
   });
   mongoose.Promise = global.Promise;
